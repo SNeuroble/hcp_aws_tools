ce5fe34c5ed0fb98b0fa766ad5690c2c1fe9008c
SNeuroble <stephanie.noble@yale.edu>
2020-02-22 20:08:51 -0500

    Fixed config paths

--- config/cfg.sh
+++ config/cfg.sh
@@ -10,8 +10,9 @@ search_str="_GSR_matrix"
 username=$(echo $USER)
 yale_bucket_key="/home/$username/.s3cmd_saved_configs/yale_bucket_access"
 source_base_dir="s3://hcpoutput/S1200/Shen268"
-target_base_dir="/home/$username/data/matrices"
-archives_dir="$local_target_archives_dir/archives/"
+remote_data_dir="/home/$username/data"
+target_base_dir="$remote_data_dir/matrices"
+archives_dir="$remote_data_dir/archives/"
 
 encoding=('LR' 'RL')
 subIDs_prefix='_subIDs.txt'
@@ -19,16 +20,17 @@ subIDs_prefix='_subIDs.txt'
 ## For copying remote to local
 
 # latest instance IP address
-un="ec2-user"
-ip="18.206.222.203"
+un="ubuntu"
+ip="52.91.82.150"
 key_file="/mridata2/home2/smn33/.ssh/MRCInstance1.pem"
 
 # data folders
+local_data_dir='/data15/mri_group/smn33_data/hcp_1200'
+
+local_source_base_dir="$local_data_dir/mnt2"
 mounted_source_dir=${target_base_dir}
 mounted_archives_dir="${source_base_dir}/archives/"
 
-local_source_base_dir="${local_target_archives_dir}mnt2"
-local_target_archives_dir='/data15/mri_group/smn33_data/hcp_1200/archives/';
-local_target_unzipped_dir='/data15/mri_group/smn33_data/hcp_1200/matrices/';
-
+local_target_archives_dir="$local_data_dir/archives/"
+local_target_unzipped_dir="$local_data_dir/matrices/"
 
