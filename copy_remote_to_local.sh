d9cf84db82bb9914194ebcefd5aa8813a24a7331
SNeuroble <stephanie.noble@yale.edu>
2020-02-23 00:10:56 -0500

    Refined copy script

--- copy_remote_to_local.sh
+++ copy_remote_to_local.sh
@@ -25,18 +25,19 @@ for i in "${!task[@]}"; do
         if [ ! -f "${main_zipfile_prefix}.zip" ]; then
             printf "Copying ${main_zipfile_prefix}* zipfiles.\n"
             cp ${mounted_archives_dir}/${this_scan}*.z* $local_target_archives_dir
-            cp $subIDs_file $local_target_archives_dir
+            cp $subIDs_file $local_target_data_dir
         fi
 
-        if [ ! -d "${local_target_archives_dir}${mounted_source_dir}/${this_scan}" ]; then
-            printf "Combining ${main_zipfile_prefix}* zip multifile, unzipping, and moving to ${local_target_archives_dir}/${this_scan}.\n"
+        local_unzipped_dir="${local_target_archives_dir}${target_base_dir}/"
+        if [ ! -d "${local_unzipped_dir}" ]; then
+            printf "Combining ${main_zipfile_prefix}* zip multifile, unzipping, and moving to ${local_unzipped_task_dir}.\n"
             zip -s 0 "${main_zipfile_prefix}.zip" --out "${main_zipfile_prefix}-unsplit.zip"
-            unzip "${main_zipfile_prefix}-unsplit.zip"
+            unzip "${main_zipfile_prefix}-unsplit.zip" -d "${local_target_archives_dir}/"
         fi
 
-        if [ ! -d "${local_target_unzipped_dir}/${this_scan}" ]; then
-            mv "${local_target_archives_dir}/${this_scan}" "${local_target_unzipped_dir}/"
-            mv "${local_target_archives_dir}/${this_scan}/${subIDs_prefix}" "${local_target_unzipped_dir}/"
+        if [ ! -d "${local_target_task_dir}" ]; then
+            mv "${local_unzipped_dir}/" "${local_target_data_dir}"
+            #mv "${local_target_archives_dir}/${this_scan}${subIDs_prefix}" "${local_target_data_dir}/"
         fi
     
     #done
