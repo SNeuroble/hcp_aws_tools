fc3dfac1e05e9a98502be192acbede060e63b536
SNeuroble <stephanie.noble@yale.edu>
2020-02-23 01:26:31 +0000

    Updating config paths

--- config/cfg.sh
+++ config/cfg.sh
@@ -1,18 +1,19 @@
 ## For getting and preparing data on remote
 
 # scan info
-task=('EMOTION' 'GAMBLING' 'SOCIAL' 'MOTOR' 'WM' 'RELATIONAL')
+task=('REST' 'RELATIONAL')
+#task=('EMOTION' 'GAMBLING' 'SOCIAL' 'MOTOR' 'WM' 'RELATIONAL')
 #task=('REST' 'LANGUAGE' 'EMOTION' 'GAMBLING' 'SOCIAL' 'MOTOR' 'WM' 'RELATIONAL')
 encoding=('LR' 'RL')
 search_str="_GSR_matrix"
 
 # paths
-username=$(echo $USER)
-yale_bucket_key="/home/$username/.s3cmd_saved_configs/yale_bucket_access"
+remote_user="ubuntu"
+yale_bucket_key="/home/${remote_user}/.s3cmd_saved_configs/yale_bucket_access"
 source_base_dir="s3://hcpoutput/S1200/Shen268"
-remote_data_dir="/home/$username/data"
+remote_data_dir="/home/${remote_user}/data"
 target_base_dir="$remote_data_dir/matrices"
-archives_dir="$remote_data_dir/archives/"
+archives_dir="$target_base_dir/archives/"
 
 encoding=('LR' 'RL')
 subIDs_prefix='_subIDs.txt'
@@ -20,7 +21,7 @@ subIDs_prefix='_subIDs.txt'
 ## For copying remote to local
 
 # latest instance IP address
-un="ubuntu"
+un=${remote_user}
 ip="52.91.82.150"
 key_file="/mridata2/home2/smn33/.ssh/MRCInstance1.pem"
 
