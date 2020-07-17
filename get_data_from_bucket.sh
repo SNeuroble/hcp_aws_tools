6911a3ebb833d47ccb4311cf7557b9054f0857f2
SNeuroble <stephanie.noble@yale.edu>
2020-07-17 00:05:42 -0400

    Updated for more general use

--- get_data_from_bucket.sh
+++ get_data_from_bucket.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# copy data from bucket to local AWS instance
+# copy data from bucket to local server
 
 # config file
 source $1
 
-mkdir -p $archives_dir
 for j in "${!encoding[@]}"; do
 
     for i in "${!task[@]}"; do
@@ -14,21 +13,20 @@ for j in "${!encoding[@]}"; do
         target_dir="${target_base_dir}/${this_scan}/"
         filenames="${target_base_dir}/${this_scan}_filenames.txt"
         subIDs_file="${target_base_dir}/${this_scan}${subIDs_prefix}"
-        zip_file="${archives_dir}/${this_scan}_archive.zip"
 
 
             mkdir -p $target_dir
 
             # record filenames to copy
             tmpfile=$(mktemp /tmp/tmp_names_${this_scan}.XXXXXX)
-            s3cmd -c ${yale_bucket_key} ls ${source_dir} > $tmpfile
+            $s3cmd -c ${bucket_key} ls ${source_dir} > $tmpfile
             grep ${search_str} $tmpfile > "${filenames}"
             sed -e "s#^20.*s3#s3#g" -i "${filenames}"
 
             # copy files
             printf "Copying data from $source_dir to ${target_dir}.\n"
             while read -r line; do
-                s3cmd -c ${yale_bucket_key} get $line "${target_dir}";
+                s3cmd -c ${bucket_key} get $line "${target_dir}";
             done < "${filenames}"
         
             # make subIDs file
